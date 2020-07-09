d6497f36c62d1d78377c99f4017acf42e517cbbe
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-09 17:16:48 +0800

    Initial Commit

---
+++ Shared/Neumo_App.swift
@@ -0,0 +1,17 @@
+//
+//  Neumo_App.swift
+//  Shared
+//
+//  Created by Shane Leigh on 09/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct Neumo_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
